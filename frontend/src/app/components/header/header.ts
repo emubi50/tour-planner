b1b67aa403f84ac40d159a99ca8d1e03de0dafcc
emubi50 <70440412+emubi50@users.noreply.github.com>
2026-04-01 23:55:22 +0200

    split nav items into separate component

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NavItem } from './nav-item/nav-item';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NavItem],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  navItems = [
+    { label: 'Tours', routerLink: '/tours' },
+    { label: 'My Tours', routerLink: '/my' },
+    { label: 'Create Tours', routerLink: '/create' },
+  ]
 }
---
+++ frontend/src/app/components/header/nav-item/nav-item.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-item',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nav-item.html',
+  styleUrl: './nav-item.css',
+})
+export class NavItem {
+  label = input('NavItem');
+  routerLink = input('routerLink');
+  isActive = input(true);
+}
