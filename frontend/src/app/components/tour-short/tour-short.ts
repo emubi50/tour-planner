e9551ddf09d09bbd589f351844fb3fe3734dd1a9
Lux-i <lucjan.lubomski@gmail.com>
2026-04-02 12:47:28 +0200

    Reverted lodash to version 4.17.23 because of a high severity vulnerability found in 4.18.1
    Created TourShort and Tour component (not implemented)

---
+++ frontend/src/app/components/tour-short/tour-short.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-short',
+  imports: [],
+  templateUrl: './tour-short.html',
+  styleUrl: './tour-short.css',
+})
+export class TourShort {
+
+}
