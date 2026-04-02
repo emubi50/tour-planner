701c3052ca11ad8e6f706b46d621fde85af44251
emubi50 <70440412+emubi50@users.noreply.github.com>
2026-04-02 23:51:30 +0200

    Added empty Login Page Component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
 import { Tour } from './pages/tour/tour';
 import { TourShort } from './components/tour-short/tour-short';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'login', component: Login },
   { path: 'tours', component: Tour },
   { path: 'tourShortTest', component: TourShort },
   { path: '**', redirectTo: '' },
