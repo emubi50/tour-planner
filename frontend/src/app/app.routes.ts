482b6c8eb08df820e70e343a62463b9a3b3432c3
emubi50 <70440412+emubi50@users.noreply.github.com>
2026-04-03 14:21:19 +0200

    Added basic login and registration layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
+import { Registration } from './pages/registration/registration';
 import { Tour } from './pages/tour/tour';
 import { TourShort } from './components/tour-short/tour-short';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
+  { path: 'registration', component: Registration },
   { path: 'tours', component: Tour },
   { path: 'tourShortTest', component: TourShort },
   { path: '**', redirectTo: '' },
