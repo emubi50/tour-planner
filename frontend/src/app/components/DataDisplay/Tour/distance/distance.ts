6b69d4e78cb7add100442e1a81aacee1f1173817
Lux-i <lucjan.lubomski@gmail.com>
2026-04-05 23:22:48 +0200

    (fix): rare case of tag-text wrapping itself instead of container handling scroll
    (ref): Change duration and distance wrappers to use lucide icons instead of text labels

--- frontend/src/app/components/DataDisplay/Tour/distance/distance.ts
+++ frontend/src/app/components/DataDisplay/Tour/distance/distance.ts
@@ -1,14 +1,17 @@
 import { Component, input } from '@angular/core';
 import { LabelMetricSet } from '../../label-metric-set/label-metric-set';
+import { LucideRoute } from '@lucide/angular';
 
 @Component({
   selector: 'app-distance',
   standalone: true,
-  imports: [LabelMetricSet],
+  imports: [LabelMetricSet, LucideRoute],
   templateUrl: './distance.html',
   styleUrl: './distance.css',
 })
 export class Distance {
+  // Same as duration ->
+  // IMPLEMENT TOGGLING ICON AND/OR TEXT ON OFF
   distance = input<string | number>(0);
 
   // Also wrap styling passthrough to base component
--- frontend/src/app/components/DataDisplay/Tour/duration/duration.ts
+++ frontend/src/app/components/DataDisplay/Tour/duration/duration.ts
@@ -1,14 +1,16 @@
 import { Component, input } from '@angular/core';
 import { LabelMetricSet } from '../../label-metric-set/label-metric-set';
+import { LucideTimer } from '@lucide/angular';
 
 @Component({
   selector: 'app-duration',
   standalone: true,
-  imports: [LabelMetricSet],
+  imports: [LabelMetricSet, LucideTimer],
   templateUrl: './duration.html',
   styleUrl: './duration.css',
 })
 export class Duration {
+  // !!FOR LATER!! Implement toggling icon and/or text on off
   duration = input<string | number>(0);
 
   // Also wrap styling passthrough to base component
