b7ed4c03ec96eceb62b7797d09b4b46a5a08e4d9
emubi50 <70440412+emubi50@users.noreply.github.com>
2026-07-01 18:44:20 +0200

    feat(migration): added RouteInformation to Tours Table in DB

---
+++ backend/TourPlanner.Dal/Migrations/20260701164018_AddRouteInformationToToursTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourPlanner.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRouteInformationToToursTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RouteInformation",
+                table: "Tours",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalTime",
+                table: "TourLogs",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalDistance",
+                table: "TourLogs",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RouteInformation",
+                table: "Tours");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalTime",
+                table: "TourLogs",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalDistance",
+                table: "TourLogs",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
